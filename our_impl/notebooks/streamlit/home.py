10f61385e62e5fb1c1f624e8a6c3387879a86cf9
Anushk97 <anushk.farkiya@gmail.com>
2024-03-22 01:40:22 +0800

    merged into main

--- our_impl/notebooks/streamlit/home.py
+++ our_impl/notebooks/streamlit/home.py
@@ -7,20 +7,51 @@ from collections import OrderedDict
 #from helper_demand_functions import *
 from utils import utils
 
+def local_css(file_name):
+    with open(file_name) as f:
+        css = f.read()
+        
+    return css
+
+st.set_page_config(
+    page_title="Promotion optimizer",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+
 st.title('Promotion optimization for retail stores')
 #st.set_page_config(page_title = "AI Planning") 
+#st.sidebar.success("navigation bar")
 
-st.sidebar.success("navigation bar")
-st.image('cover_img.png')
 
-st.header('How to use this application')
+st.subheader('How to use this application')
 st.markdown("""
-    1. Upload Data
-    2. Run Demand Function to generate demand predictions
-    3. Run GA to calculate predicted costs and profits
+    1. Upload your store datasets
+    2. Optimize 
     """)
 
 utils.add_logo()
+# with open('./style.css') as f:
+#     css = f.read()
+#     #print(css)
+    
+st.subheader("Sample dataset format for store")
+sample_data = pd.read_csv('./combined_milk_final.csv')
+st.write(sample_data)
+csv = sample_data.to_csv(index=False)
+
+st.download_button(
+    label="Download data as CSV",
+    data=csv,
+    file_name='sample_data.csv',
+    mime='text/csv',
+)
+
+css = local_css("./style.css")
+st.markdown(f'<style>{css}</style>', unsafe_allow_html=True)
+
+
 
 # CUSTOM_CSS = """
 # <style>
